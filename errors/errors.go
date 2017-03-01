823608dbc00910cd1dcf8da3f5339b1f4fa465c6
Focinfi <focinfi@gmail.com>
2017-03-01 12:04:48 +0800

    Add storage/queue.go logic

--- errors/errors.go
+++ errors/errors.go
@@ -1,17 +1,89 @@
 package errors
 
 import (
-	"errors"
+	"fmt"
 )
 
-// ErrDuplicateQueue error for duplicate queue
-var ErrDuplicateQueue = errors.New("duplicate queue")
+// DuplicateQueue error for duplicate queue
+var DuplicateQueue = NewBizErr("duplicate queue")
 
 // ErrUserNotFound error for unknown user
-var ErrUserNotFound = errors.New("user not found")
+var ErrUserNotFound = NewBizErr("user not found")
 
-// ErrQueueNotFound error for unknown queue
-var ErrQueueNotFound = errors.New("queue not found")
+// QueueNotFound error for unknown queue
+var QueueNotFound = NewBizErr("queue not found")
 
-// ErrMessageNotFound error for unknown message
-var ErrMessageNotFound = errors.New("message not found")
+// MessageNotFound error for unknown message
+var MessageNotFound = NewBizErr("message not found")
+
+// DataLost returns a internal error for losting data
+func DataLost(key string) error {
+	return NewInternalErr(fmt.Sprintf("data lost: key= %s", key))
+}
+
+// DataBroken returns a internal error for broken data
+func DataBroken(key string) error {
+	return NewInternalErr(fmt.Sprintf("data broken: key= %s", key))
+}
+
+// Biz detects the biz errors
+type Biz interface {
+	error
+	isBiz() bool
+}
+
+// IsBiz for detecting if err is Biz
+func IsBiz(err error) bool {
+	_, ok := err.(Biz)
+	return ok
+}
+
+// Internal detects the internal errors
+type Internal interface {
+	error
+	isInternal() bool
+}
+
+// IsInternal for detecting if err is Internal
+func IsInternal(err error) bool {
+	_, ok := err.(Internal)
+	return ok
+}
+
+type bizErr struct {
+	message string
+}
+
+// NewBizErr returns a new bizErr
+func NewBizErr(message string) Biz {
+	return &bizErr{
+		message: message,
+	}
+}
+
+func (err bizErr) Error() string {
+	return err.message
+}
+
+func (err bizErr) isBiz() bool {
+	return true
+}
+
+type internalErr struct {
+	message string
+}
+
+// NewInternalErr returns a new internalErr
+func NewInternalErr(message string) Internal {
+	return &internalErr{
+		message: message,
+	}
+}
+
+func (err internalErr) Error() string {
+	return err.message
+}
+
+func (err internalErr) isInternal() bool {
+	return true
+}
--- storage/queue.go
+++ storage/queue.go
@@ -1,6 +1,22 @@
 package storage
 
-import "github.com/Focinfi/sqs/models"
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/Focinfi/sqs/errors"
+	"github.com/Focinfi/sqs/models"
+)
+
+const queueKeyPrefix = "sqs.queue."
+
+func queueListKey(userID int64) string {
+	return fmt.Sprintf("%s%d", queueKeyPrefix, userID)
+}
+
+func queueKey(userID int64, queueName string) string {
+	return fmt.Sprintf("%s%d.%s", queueKeyPrefix, userID, queueName)
+}
 
 // Queue stores data
 type Queue struct {
@@ -8,22 +24,103 @@ type Queue struct {
 }
 
 // All returns queue map for userID
-func (s *Queue) All(userID int64) (map[string]*models.Queue, error) {
-	return nil, nil
+func (s *Queue) All(userID int64) ([]models.Queue, error) {
+	all := []models.Queue{}
+	key := queueListKey(userID)
+
+	val, ok := s.db.Get(key)
+	if !ok {
+		return nil, errors.ErrUserNotFound
+	}
+
+	if val == "" {
+		return all, nil
+	}
+
+	if err := json.Unmarshal([]byte(val), &all); err != nil {
+		return nil, errors.DataBroken(key)
+	}
+
+	return all, nil
 }
 
 // One returns a queue for the userID with the queueName
 func (s *Queue) One(userID int64, queueName string) (*models.Queue, error) {
-	return nil, nil
+	all, err := s.All(userID)
+	if err != nil {
+		return nil, err
+	}
+
+	var theQueue *models.Queue
+	for _, queue := range all {
+		if queue.Name == queueName {
+			*theQueue = queue
+		}
+	}
+
+	if theQueue == nil {
+		return nil, errors.QueueNotFound
+	}
+
+	return theQueue, nil
 }
 
 // Add add q for userID
 func (s *Queue) Add(q *models.Queue) error {
+	all, err := s.All(q.UserID)
+	if err != nil {
+		return err
+	}
+
+	for _, queue := range all {
+		if queue.Name == q.Name {
+			return errors.DuplicateQueue
+		}
+	}
+
+	all = append(all, *q)
+	data, err := json.Marshal(all)
+	if err != nil {
+		return errors.NewInternalErr(err.Error())
+	}
+
+	err = s.db.Put(queueListKey(q.UserID), string(data))
+	if err != nil {
+		return errors.NewInternalErr(err.Error())
+	}
+
 	return nil
 }
 
 // Remove removes the queue with the name
 func (s *Queue) Remove(userID int64, queueName string) error {
+	all, err := s.All(userID)
+	if err != nil {
+		return err
+	}
+
+	index := -1
+	for i, queue := range all {
+		if queue.Name == queueName {
+			index = i
+		}
+	}
+
+	if index < 0 {
+		return errors.QueueNotFound
+	}
+
+	all = append(all[:index], all[index+1:]...)
+	data, err := json.Marshal(all)
+	if err != nil {
+		return errors.NewInternalErr(err.Error())
+	}
+
+	err = s.db.Put(queueListKey(userID), string(data))
+	if err != nil {
+		return errors.NewInternalErr(err.Error())
+	}
+
 	return nil
 }
 
