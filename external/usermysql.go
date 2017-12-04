11561d3bcbd2af336bb260ec57cdd36ec7e4153e
Focinfi <focinfi@gmail.com>
2017-12-04 11:37:30 +0800

    Update logger

--- external/usermysql.go
+++ external/usermysql.go
@@ -18,7 +18,7 @@ func init() {
 	dbCfg := config.Config.SQLDB
 	db, err := gorm.Open("mysql", fmt.Sprintf("%s:%s@%s(%s:%s)/%s?charset=utf8&parseTime=True&loc=UTC", dbCfg.User, dbCfg.Password, dbCfg.Protocol, dbCfg.Host, dbCfg.Port, dbCfg.Name))
 	if err != nil {
-		panic(db)
+		panic(err)
 	}
 
 	gormDB = db
