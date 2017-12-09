b743f436746f6a923c6691d91f691805741634d9
Focinfi <focinfi@gmail.com>
2017-12-09 13:52:18 +0800

    Update urlutil

--- util/urlutil/urlutil.go
+++ util/urlutil/urlutil.go
@@ -3,17 +3,24 @@ package urlutil
 import (
 	"fmt"
 	"strings"
+
+	"github.com/Focinfi/gosqs/config"
 )
 
 // MakeURL make a url for some abbreviation addr like ":12345"
 // TODO: fit in more scene
 func MakeURL(addr string) string {
+	protocol := "http"
+	if config.Config.Env.IsProduction() {
+		protocol = "https"
+	}
+
 	if strings.HasPrefix(addr, ":") {
-		return fmt.Sprintf("http://127.0.0.1%s", addr)
+		return fmt.Sprintf("%s://127.0.0.1%s", protocol, addr)
 	}
 
 	if !strings.HasPrefix(addr, "http://") && !strings.HasPrefix(addr, "https://") {
-		return fmt.Sprintf("http://%s", addr)
+		return fmt.Sprintf("%s://%s", protocol, addr)
 	}
 
 	return addr
